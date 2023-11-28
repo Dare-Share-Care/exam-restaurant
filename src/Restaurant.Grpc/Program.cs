c5860d6eb5d70b78e0a917f3b76cce4cbe2a91ef
Janussr <Janusstivang@gmail.com>
2023-11-28 17:56:24 +0100

    grpc sends restaurant id and menus

--- src/Restaurant.Grpc/Program.cs
+++ src/Restaurant.Grpc/Program.cs
@@ -1,12 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Restaurant.Core.Interfaces;
+using Restaurant.Core.Services;
 using Restaurant.Grpc.Services;
+using Restaurant.Infrastructure.Data;
+using Restaurant.Infrastructure.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
+//DBContext
+builder.Services.AddDbContext<RestaurantContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DbContext"));
+});
+
+//Build services
+builder.Services.AddScoped<IRestaurantService, RestaurantService>();
+
+//Build repositories
+builder.Services.AddScoped(typeof(IReadRepository<>), typeof(EfRepository<>));
+builder.Services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+
 // Add services to the container.
 builder.Services.AddGrpc();
+
 
 var app = builder.Build();
 
--- src/Restaurant.Grpc/Services/CatalogueService.cs
+++ src/Restaurant.Grpc/Services/CatalogueService.cs
@@ -1,37 +1,46 @@
 ﻿using Grpc.Core;
+using Restaurant.Core.Interfaces;
 
 namespace Restaurant.Grpc.Services
 {
     public class CatalogueService : Catalogue.CatalogueBase
     {
-        private readonly IServiceProvider _serviceProvider;
-
-        public CatalogueService(IServiceProvider serviceProvider)
+        private readonly IRestaurantService _restaurantService;
+        
+        public CatalogueService(IRestaurantService restaurantService)
         {
-            _serviceProvider = serviceProvider;
+            _restaurantService = restaurantService;
         }
-
-        public override async Task<RestaurantResponse> SendCatalogue(RestaurantRequest request, ServerCallContext context)
+        public override async Task<RestaurantResponse> SendCatalogue(RestaurantRequest request,
+            ServerCallContext context)
         {
-                var restaurantResponse = new RestaurantResponse
-                {
-                    Restaurant = new Restauranten
-                    {
-                        Id = 1,
-                        Menu = { }
-                    }
-                };
-               
-                    var menuItemProto = new MenuItem
-                    {
-                        Id = 1,
-                        Name = "banan",
-                        Price = 25
-                    };
-                    
-                    restaurantResponse.Restaurant.Menu.Add(menuItemProto);
-                
-                return restaurantResponse;
+            // Fetch the restaurants
+            var restaurants = await _restaurantService.GetAllRestaurantsAsync();
+            // Find the restaurant matching the request
+            var restaurant = restaurants.FirstOrDefault(r => r.Id == request.RestaurantId);
+            
+            // Get menu 
+            var menu = await _restaurantService.GetRestaurantMenuAsync(request.RestaurantId);
+            restaurant.Menu = menu;
+            
+            // Check if the restaurant was found
+            if (restaurant != null)
+            {
+                // Convert menu items to protobuf format
+                var menuItems = restaurant.Menu.Select(item => new MenuItem
+                    { Id = item.Id, Name = item.Name, Price = (float)item.Price }).ToList();
+
+                // Create the restaurant protobuf message
+                var restaurantMessage = new Restauranten { Id = restaurant.Id, Menu = { menuItems } };
+
+                // Return the response
+                return new RestaurantResponse { Restaurant = restaurantMessage };
+            }
+            else
+            {
+                // Restaurant not found, return an empty response or throw an error
+                return new RestaurantResponse();
             }
         }
     }
+}
