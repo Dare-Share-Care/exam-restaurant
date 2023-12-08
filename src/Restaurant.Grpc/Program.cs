a9ad73cceb0e5265c99b40b77e5aad39b0f9610a
FrederikBA <frederikbandersen98@gmail.com>
2023-12-08 11:45:43 +0100

    bugfix: dependency injeciton grpc service

--- src/Restaurant.Grpc/Program.cs
+++ src/Restaurant.Grpc/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddDbContext<RestaurantContext>(options =>
 
 //Build services
 builder.Services.AddScoped<IRestaurantService, RestaurantService>();
+builder.Services.AddScoped<ILoggingService, LoggingService>();
 
 //Build repositories
 builder.Services.AddScoped(typeof(IReadRepository<>), typeof(EfRepository<>));
